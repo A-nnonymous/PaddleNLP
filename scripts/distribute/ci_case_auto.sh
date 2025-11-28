7a00d2c02068e4921f627c793614b00b97d0b277
zrr1999 <2742392377@qq.com>
2025-11-28 10:25:22 +0000

    disable llm_gpt_dygraph_auto_bs8_fp32_DP2-MP2

--- scripts/distribute/ci_case_auto.sh
+++ scripts/distribute/ci_case_auto.sh
@@ -172,7 +172,7 @@ function llm_gpt_case_list_auto() {
         # The test name must have "llm_gpt_dygraph_auto_" as a prefix, 
         # which will be used for tracking the execution status of the case.
         llm_gpt_dygraph_auto_bs8_fp32_DP2
-        llm_gpt_dygraph_auto_bs8_fp32_DP2-MP2
+        # llm_gpt_dygraph_auto_bs8_fp32_DP2-MP2
         llm_gpt_dygraph_auto_bs8_fp32_DP2-MP2-PP2
         llm_gpt_dygraph_auto_bs8_fp16_DP2-MP2-PP2
         llm_gpt_dygraph_auto_bs8_fp16_DP2-MP2-PP2_intermediate
